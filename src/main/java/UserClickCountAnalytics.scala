92ee0c567ecdeb4480a8c565b189849423c64c33
supermy <springclick@gmail.com>
2017-10-30 09:35:23 +0800

    Redis 采用 单例+pipeline,性能提升10倍

--- src/main/java/UserClickCountAnalytics.scala
+++ src/main/java/UserClickCountAnalytics.scala
@@ -45,6 +45,69 @@ object UserClickCountAnalytics {
     val userClicks = events.map(x => (x.get("uid").getAsString, x.get("click_count").getAsInt)).reduceByKey(_ + _)
     userClicks.foreachRDD(rdd => {
       rdd.foreachPartition(partitionOfRecords => {
+
+        //集群模式+pipeline
+
+        /**
+          * Internal Redis client for managing Redis connection {@link Jedis} based on {@link RedisPool}
+          */
+        object InternalRedisClient extends Serializable {
+
+          @transient private var pool: JedisPool = null
+
+          def makePool(redisHost: String, redisPort: Int, redisTimeout: Int,
+                       maxTotal: Int, maxIdle: Int, minIdle: Int): Unit = {
+            makePool(redisHost, redisPort, redisTimeout, maxTotal, maxIdle, minIdle, true, false, 10000)
+          }
+
+          //scala 的单例模式
+          def makePool(redisHost: String, redisPort: Int, redisTimeout: Int,
+                       maxTotal: Int, maxIdle: Int, minIdle: Int, testOnBorrow: Boolean,
+
+                       testOnReturn: Boolean, maxWaitMillis: Long): Unit = {
+
+            if(pool == null) {
+
+              val poolConfig = new GenericObjectPoolConfig()
+
+              poolConfig.setMaxTotal(maxTotal)
+
+              poolConfig.setMaxIdle(maxIdle)
+
+              poolConfig.setMinIdle(minIdle)
+
+              poolConfig.setTestOnBorrow(testOnBorrow)
+              poolConfig.setTestOnReturn(testOnReturn)
+              poolConfig.setMaxWaitMillis(maxWaitMillis)
+              pool = new JedisPool(poolConfig, redisHost, redisPort, redisTimeout)
+              val hook = new Thread{
+                override def run = pool.destroy()
+              }
+              sys.addShutdownHook(hook.run)
+            }
+          }
+          def getPool: JedisPool = {
+            assert(pool != null)
+            pool
+          }
+        }
+        // Redis configurations
+        val maxTotal = 10
+        val maxIdle = 10
+        val minIdle = 1
+        val redisHost = "127.0.0.1"
+        val redisPort = 6379
+        val redisTimeout = 30000
+        val dbIndex = 1
+
+        InternalRedisClient.makePool(redisHost, redisPort, redisTimeout, maxTotal, maxIdle, minIdle)
+
+        val jedis =InternalRedisClient.getPool.getResource
+        jedis.select(dbIndex)
+
+        val p = jedis.pipelined()
+
+
         partitionOfRecords.foreach(pair => {
 //          val uid = pair._1
 //          val clickCount = pair._2
@@ -55,70 +118,17 @@ object UserClickCountAnalytics {
 //          jedis.hincrBy(clickHashKey, uid, clickCount)
 //          RedisClient.pool.returnResource(jedis)
 
-          //集群模式
-
-          /**
-            * Internal Redis client for managing Redis connection {@link Jedis} based on {@link RedisPool}
-            */
-          object InternalRedisClient extends Serializable {
-
-            @transient private var pool: JedisPool = null
-
-            def makePool(redisHost: String, redisPort: Int, redisTimeout: Int,
-            maxTotal: Int, maxIdle: Int, minIdle: Int): Unit = {
-              makePool(redisHost, redisPort, redisTimeout, maxTotal, maxIdle, minIdle, true, false, 10000)
-            }
-
-            def makePool(redisHost: String, redisPort: Int, redisTimeout: Int,
-            maxTotal: Int, maxIdle: Int, minIdle: Int, testOnBorrow: Boolean,
-
-            testOnReturn: Boolean, maxWaitMillis: Long): Unit = {
-
-              if(pool == null) {
-
-                val poolConfig = new GenericObjectPoolConfig()
-
-                poolConfig.setMaxTotal(maxTotal)
-
-                poolConfig.setMaxIdle(maxIdle)
-
-                poolConfig.setMinIdle(minIdle)
-
-                poolConfig.setTestOnBorrow(testOnBorrow)
-                poolConfig.setTestOnReturn(testOnReturn)
-                poolConfig.setMaxWaitMillis(maxWaitMillis)
-                pool = new JedisPool(poolConfig, redisHost, redisPort, redisTimeout)
-                val hook = new Thread{
-                  override def run = pool.destroy()
-                }
-                sys.addShutdownHook(hook.run)
-              }
-            }
-            def getPool: JedisPool = {
-              assert(pool != null)
-              pool
-            }
-          }
-          // Redis configurations
-          val maxTotal = 10
-          val maxIdle = 10
-          val minIdle = 1
-          val redisHost = "127.0.0.1"
-          val redisPort = 6379
-          val redisTimeout = 30000
-          val dbIndex = 1
-
-          InternalRedisClient.makePool(redisHost, redisPort, redisTimeout, maxTotal, maxIdle, minIdle)
-
           val uid = pair._1
           val clickCount = pair._2
-          val jedis =InternalRedisClient.getPool.getResource
-          jedis.select(dbIndex)
           jedis.hincrBy(clickHashKey, uid, clickCount)
-          InternalRedisClient.getPool.returnResource(jedis)
 
 
         })
+
+
+        p.sync()
+        InternalRedisClient.getPool.returnResource(jedis)
+
       })
     })
 
